65dbb1ec62fbc90a5f2ab9747b5eb6c4c5d8054b
Kosean <kosean2211@gmail.com>
2024-11-21 18:49:44 -0600

    Primer commit

--- 1) Programacion Python/ej.py
+++
@@ -1,5 +0,0 @@
-try:
-    # lanzamos un error con un mensaje personalizado
-    raise ValueError("Este es un error personalizado.")
-except ValueError as e:
-    print(f"Error: {e}")
--- 2) Python con Raspberrypi/api.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
-
-print(response.status_code)
-print(response.json().get('abilities'))
